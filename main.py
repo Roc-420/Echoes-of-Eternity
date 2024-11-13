9a602ccaad9459fef9887c76da6907ad550c5e9a
MrPurple17 <1044413@pdsb.net>
2024-11-13 01:37:35 -0500

    Added rug system main.py

--- main.py
+++ main.py
@@ -1,43 +1,45 @@
-import pygame as pg
+import pygame
 
-# Initialize pg and create window
-pg.init()
+# Initialize pygame and create window
+pygame.init()
 DISPLAY_W, DISPLAY_H = 1700, 1200
-canvas = pg.Surface((DISPLAY_W, DISPLAY_H))
-window = pg.display.set_mode((DISPLAY_W, DISPLAY_H))
+canvas = pygame.Surface((DISPLAY_W, DISPLAY_H))
+window = pygame.display.set_mode((DISPLAY_W, DISPLAY_H))
 running = True
 
 #takes images from directory (we need to learn how to use spritesheets instead of this)
-ava_right1 = pg.image.load('Ava/Ava_right1.png').convert_alpha()
-ava_right2 = pg.image.load('Ava/Ava_right2.png').convert_alpha()
-ava_right3 = pg.image.load('Ava/Ava_right3.png').convert_alpha()
-ava_right4 = pg.image.load('Ava/Ava_right4.png').convert_alpha()
-ava_right5 = pg.image.load('Ava/Ava_right5.png').convert_alpha()
-ava_right6 = pg.image.load('Ava/Ava_right6.png').convert_alpha()
+ava_right1 = pygame.image.load('Ava/Ava_right1.png').convert_alpha()
+ava_right2 = pygame.image.load('Ava/Ava_right2.png').convert_alpha()
+ava_right3 = pygame.image.load('Ava/Ava_right3.png').convert_alpha()
+ava_right4 = pygame.image.load('Ava/Ava_right4.png').convert_alpha()
+ava_right5 = pygame.image.load('Ava/Ava_right5.png').convert_alpha()
+ava_right6 = pygame.image.load('Ava/Ava_right6.png').convert_alpha()
 
-ava_left1 = pg.image.load('Ava/Ava_left1.png').convert_alpha()
-ava_left2 = pg.image.load('Ava/Ava_left2.png').convert_alpha()
-ava_left3 = pg.image.load('Ava/Ava_left3.png').convert_alpha()
-ava_left4 = pg.image.load('Ava/Ava_left4.png').convert_alpha()
-ava_left5 = pg.image.load('Ava/Ava_left5.png').convert_alpha()
-ava_left6 = pg.image.load('Ava/Ava_left6.png').convert_alpha()
+ava_left1 = pygame.image.load('Ava/Ava_left1.png').convert_alpha()
+ava_left2 = pygame.image.load('Ava/Ava_left2.png').convert_alpha()
+ava_left3 = pygame.image.load('Ava/Ava_left3.png').convert_alpha()
+ava_left4 = pygame.image.load('Ava/Ava_left4.png').convert_alpha()
+ava_left5 = pygame.image.load('Ava/Ava_left5.png').convert_alpha()
+ava_left6 = pygame.image.load('Ava/Ava_left6.png').convert_alpha()
 
-ava_up1 = pg.image.load('Ava/Ava_up1.png').convert_alpha()
-ava_up2 = pg.image.load('Ava/Ava_up2.png').convert_alpha()
-ava_up3 = pg.image.load('Ava/Ava_up3.png').convert_alpha()
-ava_up4 = pg.image.load('Ava/Ava_up4.png').convert_alpha()
-ava_up5 = pg.image.load('Ava/Ava_up5.png').convert_alpha()
-ava_up6 = pg.image.load('Ava/Ava_up6.png').convert_alpha()
+ava_up1 = pygame.image.load('Ava/Ava_up1.png').convert_alpha()
+ava_up2 = pygame.image.load('Ava/Ava_up2.png').convert_alpha()
+ava_up3 = pygame.image.load('Ava/Ava_up3.png').convert_alpha()
+ava_up4 = pygame.image.load('Ava/Ava_up4.png').convert_alpha()
+ava_up5 = pygame.image.load('Ava/Ava_up5.png').convert_alpha()
+ava_up6 = pygame.image.load('Ava/Ava_up6.png').convert_alpha()
 
-ava_down1 = pg.image.load('Ava/Ava_down1.png').convert_alpha()
-ava_down2 = pg.image.load('Ava/Ava_down2.png').convert_alpha()
-ava_down3 = pg.image.load('Ava/Ava_down3.png').convert_alpha()
-ava_down4 = pg.image.load('Ava/Ava_down4.png').convert_alpha()
-ava_down5 = pg.image.load('Ava/Ava_down5.png').convert_alpha()
-ava_down6 = pg.image.load('Ava/Ava_down6.png').convert_alpha()
+ava_down1 = pygame.image.load('Ava/Ava_down1.png').convert_alpha()
+ava_down2 = pygame.image.load('Ava/Ava_down2.png').convert_alpha()
+ava_down3 = pygame.image.load('Ava/Ava_down3.png').convert_alpha()
+ava_down4 = pygame.image.load('Ava/Ava_down4.png').convert_alpha()
+ava_down5 = pygame.image.load('Ava/Ava_down5.png').convert_alpha()
+ava_down6 = pygame.image.load('Ava/Ava_down6.png').convert_alpha()
 
-ava_idle_right = pg.image.load('Ava/Ava_idle_right.png')
-ava_idle_left = pg.image.load('Ava/Ava_idle_left.png')
+ava_idle_right = pygame.image.load('Ava/Ava_idle_right.png')
+ava_idle_left = pygame.image.load('Ava/Ava_idle_left.png')
+
+background = pygame.image.load('BG/test_pokemon_map.png')
 
 #compiles each direction into a list
 ava_right = [ava_right1, ava_right2, ava_right3, ava_right4, ava_right5, ava_right6]
@@ -52,6 +54,8 @@ ava = last_idle
 #adjustable coordinates
 avaX = DISPLAY_W/2
 avaY = DISPLAY_H/2
+
+
 
 def Offscreen_check(ifXY):
     global avaX, avaY
@@ -71,10 +75,15 @@ def Offscreen_check(ifXY):
 ava_rect = ava.get_rect(center = (avaX, avaY))
 ava.set_colorkey('White')
 
+bgX = 0
+bgY = 0
+
+background_rect = background.get_rect(center = (bgX, bgY))
+
 #to scale later
 def Ava_size():
     global ava
-    ava = pg.transform.rotozoom(ava,0,5)
+    ava = pygame.transform.rotozoom(ava,0,5)
 
 
 #if key is pressed
@@ -123,39 +132,71 @@ def Animate():
 #index for iteration
 index = 0
 
+def rug():
+    global background, avaX, avaY, bgX, bgY
+    print(bgX, bgY)
+    if avaY > DISPLAY_H-500:
+        if bgY > ((background_rect.height*5*-1)+DISPLAY_H):
+            avaY = DISPLAY_H-500
+            bgY -= 10
+            print('down')
+    
+    if avaY < 300:
+        if bgY < 0:
+            avaY = 300
+            bgY += 10
+            print('up')
+
+    if avaX < 500:
+        if bgX < 0:
+            avaX = 500
+            bgX += 10
+            print('left')
+    
+    if avaX > DISPLAY_W-500:
+        if bgX > ((background_rect.width*5*-1)+DISPLAY_W):
+            avaX = DISPLAY_W-500
+            bgX -= 10
+            print('right')
+
+background = pygame.transform.rotozoom(background,0,5)
+
+
 #initializes frame rate manager
-clock = pg.time.Clock()
+clock = pygame.time.Clock()
 
 while running:
     #quits the game if window is closed
-    for event in pg.event.get():
-        if event.type == pg.QUIT: running = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: running = False
 
     #fills screen with black so that previous frames are erased
     window.fill((0,0,0))
 
     
-    #places character
+    #places characters
+    window.blit(background, (bgX,bgY))
+    rug()
     window.blit(ava,(avaX,avaY))
 
     #listens for key press
-    keys = pg.key.get_pressed()
-    if keys[pg.K_a]:
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a]:
         avaX -=10
         print("left")
         key = True
         direction = 'left'
-    elif keys[pg.K_d]:
+    elif keys[pygame.K_d]:
         avaX +=10
         print("right")
         key = True
         direction = 'right'
-    elif keys[pg.K_w]:
+    elif keys[pygame.K_w]:
         avaY -=10
         print("up")
         key = True
         direction = 'up'
-    elif keys[pg.K_s]:
+    elif keys[pygame.K_s]:
         avaY +=10
         print("down")
         key = True
@@ -170,6 +211,6 @@ while running:
     clock.tick(60)
 
     #updates the display
-    pg.display.update()
+    pygame.display.update()
 
-pg.quit()
+pygame.quit()
