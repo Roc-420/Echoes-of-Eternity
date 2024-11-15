ae0b98aacd3c6b9dd7c01b25bac836dcbddf99b9
Roc-420 <nlast031@gmail.com>
2024-11-15 07:13:34 -0500

    create function to check for win

--- ai.py
+++ ai.py
@@ -13,6 +13,18 @@ def ai_move(bias,bias_level):
     return list[choice]
 
 
-
+def win_check(move_1,move_2):
+    list = ["rock","paper","scissors","rock"]
+    move_pos_1 = list.index(move_1)
+    move_pos_2  = list.index(move_2)
+    if move_1 == move_2:
+        print("tie!!")
+    elif move_1 == list[ move_pos_2 +1  ]:
+        print("player 1 wins!!")
+    elif move_2 == list[move_pos_1 + 1]:
+        print("player 2 wins!!")
 fe = ai_move("rock",2)
 print(fe)
+
+
+win_check("rock","rock")
